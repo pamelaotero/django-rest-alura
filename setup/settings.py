02962c1dd3dfe3bc2999cda70231aefa325b0a6f
Pamela Otéro <=>
2021-11-29 22:27:30 -0400

    [API] Criando a classe Aluno e Curso com as migrações

--- setup/settings.py
+++ setup/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'escola', # para entender que os modelos fazem parte da aplicação também
 ]
 
 MIDDLEWARE = [
