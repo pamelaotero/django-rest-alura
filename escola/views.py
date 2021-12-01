49b6fa3bf1ef84548327928d611758158b5e443a
Pamela Otéro <=>
2021-12-01 17:04:07 -0400

    [API] Adicionando Classes serializer Aluno e Curso

--- escola/views.py
+++ escola/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework import serializers, viewsets
+from escola.models import Aluno, Curso
+from escola.serializer import AlunosSerializer, CursosSerializer
 
-def alunos(request):
-    if request.method == 'GET':
-        aluno = {'id':1, 'nome':'Guilherme'}
-        return JsonResponse(aluno)
+# com essa linha de código será possível manipular todas as requisições que queremos diponibilizar
+
+class AlunosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os alunos e alunas"""
+    queryset = Aluno.objects.all()
+    serializer_class = AlunosSerializer
+
+class CursosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os cursos"""
+    queryset = Curso.objects.all()
+    serializer_class = CursosSerializer
+
+
+
